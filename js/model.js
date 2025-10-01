e89c7f1d961d301a54f9804190847e816a91cf55
GSAmundsen <92396652+GSAmundsen@users.noreply.github.com>
2025-10-01 18:08:13 +0200

    Update

---
+++ js/model.js
@@ -0,0 +1,7 @@
+let model = {
+    data: {
+        
+        tasks: []
+    },
+
+}
---
+++ js/view.js
@@ -0,0 +1,9 @@
+function updateView() {
+    document.getElementById('app') = /*html*/`
+    <div><h2>Test prosjekt</h2></div>
+    
+    
+    
+    
+    `; 
+}
